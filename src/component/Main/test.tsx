1fc8a972c7f1f7a42bc3b637cf3b71798521403d
Kreutz-56 <guilherme_vg1@hotmail.com>
2023-06-08 20:36:05 -0300

    test: new test to main with color converter fn

--- src/component/Main/test.tsx
+++ src/component/Main/test.tsx
@@ -2,6 +2,20 @@ import { render, screen } from '@testing-library/react'
 
 import { Main } from '.'
 
+export const convertHexToRGBA = (hexCode: string) => {
+  let hex = hexCode.replace('#', '')
+
+  if (hex.length === 3) {
+    hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`
+  }
+
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+
+  return { r, g, b }
+}
+
 describe('<Main />', () => {
   it('should render the header', () => {
     render(<Main />)
@@ -9,5 +23,13 @@ describe('<Main />', () => {
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      color: convertHexToRGBA('#831187')
+    })
   })
 })
