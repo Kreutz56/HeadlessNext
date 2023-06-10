d402d9803567f019d1e2bb6097ea338dce1a3e84
Kreutz-56 <guilherme_vg1@hotmail.com>
2023-06-09 21:17:35 -0300

    chore: graphQL client URL fix

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from 'graphql-request'
 
-export const client = new GraphQLClient('http://localhost:1337/graphql')
+export const client = new GraphQLClient('http://127.0.0.1:1337/graphql')
